7498fbfba3410cfed5032c4ddf955bd10a0b62ce
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-23 18:50:47 +0100

    Add init to index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Slashing!")
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length === 1) {
+    console.log(`Starting crawling at ${args[0]}...`)
+  } else {
+    console.error("Please, provide a URL to crawl.");
+  }
+}
+
+main();
