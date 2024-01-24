b1898b10ba88abcdb6908dc59c5e285b0274660f
Alex Wilkom <alex.wilkom@icloud.com>
2024-01-24 09:34:24 +0100

    Update crawlPage to include depth recursion

--- crawl.js
+++ crawl.js
@@ -1,29 +1,58 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const currentUrlObj = new URL(currentURL);
+  const baseUrlObj = new URL(baseURL);
+  if (currentUrlObj.hostname !== baseUrlObj.hostname) {
+    return pages;
+  }
+
+  const normalizedURL = normalizeURL(currentURL);
+
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
+    return pages;
+  }
+
+  if (currentURL === baseURL) {
+    // don't count the base URL as a link to itself
+    pages[normalizedURL] = 0;
+  } else {
+    pages[normalizedURL] = 1;
+  }
+
+  let htmlBody = "";
   try {
     const response = await fetch(currentURL);
     if (response.ok) {
       const contentType = response.headers.get("content-type");
-      if (contentType && !contentType.includes("text/html")) {
-        console.error(`Error: content type is not text/html`);
-      } else if (contentType && contentType.includes("text/html")) {
-        console.log(`Crawling...`);
-        console.log(await response.text());
+      if (contentType.includes("text/html")) {
+        htmlBody = await response.text();
+      } else {
+        console.log(`Error: content type is not text/html`);
+        return pages;
       }
     } else {
-      console.error(`Error: ${response.status}`);
+      console.log(`Error: ${response.status}`);
+      return pages;
     }
   } catch (error) {
-    console.error(error.message);
+    console.log(error.message);
   }
+
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+  for (const nextURL of nextURLs) {
+    pages = await crawlPage(baseURL, nextURL, pages);
+  }
+
+  return pages;
 }
 
 function normalizeURL(url) {
   try {
     const urlObj = new URL(url);
     let fullPath = `${urlObj.host}${urlObj.pathname}`;
-    if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    if (fullPath.length > 0 && fullPath[-1] === "/") {
       fullPath = fullPath.slice(0, -1);
     }
     return fullPath;
--- crawl.test.js
+++ crawl.test.js
@@ -2,22 +2,22 @@ const { normalizeURL, getURLsFromHTML } = require("./crawl");
 
 describe("normalize url", () => {
   test("should remove protocol", () => {
-    const input = "https://bendingsoons.com/path";
+    const input = "https://bendingspoons.com/path";
     const actual = normalizeURL(input);
-    const expected = "bendingsoons.com/path";
+    const expected = "bendingspoons.com/path";
     expect(actual).toEqual(expected);
   });
 
   test("should remove trailing slash", () => {
-    const input = "https://bendingsoons.com/path/";
+    const input = "https://bendingspoons.com/path";
     const actual = normalizeURL(input);
-    const expected = "bendingsoons.com/path";
+    const expected = "bendingspoons.com/path";
     expect(actual).toEqual(expected);
   });
 });
 
 describe("getURLsFromHTML", () => {
-  const baseURL = "https://www.bendingsoons.com";
+  const baseURL = "https://www.bendingspoons.com";
   const inputBody = `
     <body>
       <h1>Backend</h1>
@@ -33,7 +33,7 @@ describe("getURLsFromHTML", () => {
     const expected = [
       "https://apple.com/",
       "https://www.starwars.com/",
-      "https://www.bendingsoons.com/path/one/two/",
+      "https://www.bendingspoons.com/path/one/two/",
     ];
     expect(actual).toEqual(expected);
   });
